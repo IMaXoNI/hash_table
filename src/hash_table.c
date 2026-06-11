adddfbed20e5640706c614636201225cd40db685
MaXoN <maks05.0540@gmail.com>
2026-06-11 17:02:05 +0300

    add node_ptr->prev check

--- src/hash_table.c
+++ src/hash_table.c
@@ -119,8 +119,10 @@ hash_table_delete_key(HashTable *hashtable, const char *key)
     {
         node_ptr->next->prev = node_ptr->prev;
     }
-
-    node_ptr->prev->next = node_ptr->next;
+    if (node_ptr->prev != NULL)
+    {
+        node_ptr->prev->next = node_ptr->next;
+    }
     free(node_ptr->key);
     free(node_ptr->value);
     free(node_ptr);
