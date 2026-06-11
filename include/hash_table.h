20572d0660c180bad8de5fbc27fbb5cb049e00af
MaXoN <maks05.0540@gmail.com>
2026-06-11 14:39:45 +0300

    fill hash_table.h

---
+++ include/hash_table.h
@@ -0,0 +1,26 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <stddef.h>
+
+typedef struct Node
+{
+    char *key;
+    char *value;
+    Node *next;
+    Node *prev;
+} Node;
+
+typedef struct HashTable
+{
+    Node **table;
+    size_t size;
+} HashTable;
+
+HashTable* hash_table_create(size_t size);
+int hash_table_insert(HashTable *table, const char *key, const char *value);
+const char* hash_table_find(const HashTable *table, const char *key);
+int hash_table_delete_key(HashTable *table, const char *key);
+void hash_table_destroy_table(HashTable* table);
+
+#endif
